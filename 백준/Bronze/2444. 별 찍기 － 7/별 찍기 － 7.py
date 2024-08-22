89a43885f97508c0a55a989839bae71724baca4b
keonhee0404 <168632887+keonhee0404@users.noreply.github.com>
2024-08-22 20:53:58 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 32 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -0,0 +1,9 @@
+n=int(input())
+for i in range(n):
+    spaces=n-i-1
+    stars=2*i+1
+    print(" " * spaces+ "*" * stars)
+for i in range(n - 2, -1, -1):
+    spaces = n - i - 1
+    stars = 2 * i + 1
+    print(" " * spaces + "*" * stars)
